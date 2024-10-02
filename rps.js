249a0129249481ae8cb3c92410d60749de142d07
anandveeraswamy <anandveeraswamy@gmail.com>
2024-10-02 13:22:43 +0000

    first working version

--- rps.js
+++ rps.js
@@ -1,6 +1,29 @@
 function rockPaperScissors(player1, player2) {
+  if(player1 == player2) {    
+    return "draw";
+  }
+  
+  if( 
+      player1 == "rock" && player2 == "scissors" ||
+      player1 == "paper" && player2 == "rock" ||
+      player1 == "scissors" && player2 == "paper" ||
+      player1 == "lizard" && player2 == "spock" ||
+      player1 == "spock" && player2 == "scissors" ||
+      player1 == "rock" && player2 == "lizard" ||
+      player1 == "paper" && player2 == "spock" ||
+      player1 == "scissors" && player2 == "lizard" ||
+      player1 == "lizard" && player2 == "paper" ||
+      player1 == "spock" && player2 == "rock"
+    )
+    {
+      return "player1";     
+    }
+    
+  else {
+    return "player2"
+  }
 
-  // made a change
+  
   
    
 }
