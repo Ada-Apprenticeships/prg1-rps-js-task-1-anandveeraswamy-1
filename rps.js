f5835f6881c8bfb67f993784cd23a0d46bed6599
anandveeraswamy <anandveeraswamy@gmail.com>
2024-11-01 10:45:00 +0000

    testing changes

--- rps.js
+++ rps.js
@@ -2,31 +2,15 @@ function rockPaperScissors(player1, player2) {
   if(player1 == player2) {    
     return "draw";
   }
-  
-  if( 
-      player1 == "rock" && player2 == "scissors" ||
-      player1 == "paper" && player2 == "rock" ||
-      player1 == "scissors" && player2 == "paper" ||
-      player1 == "lizard" && player2 == "spock" ||
-      player1 == "spock" && player2 == "scissors" ||
-      player1 == "rock" && player2 == "lizard" ||
-      player1 == "paper" && player2 == "spock" ||
-      player1 == "scissors" && player2 == "lizard" ||
-      player1 == "lizard" && player2 == "paper" ||
-      player1 == "spock" && player2 == "rock"
-    )
-    {
-      return "player1";     
-    }
-    
-  else {
-    return "player2"
-  }
-
-  
-  
-   
+ 
 }
+
+let nums = [1, 2, 3]
+let nums2 = [4, 5, 6]
+let nums3 = [7, 8, 9]
+let concatArr = nums.concat(nums3, nums2)
+console.log(concatArr)
+// made some changes
 
 
 
